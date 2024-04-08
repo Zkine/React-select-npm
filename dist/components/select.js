89c16cc324bd135f9ab0ab577853749531295be9
Zkine <philippe_jaymes@hotmail.com>
2024-04-08 18:29:21 +0200

    le pakage dist est mis à jour et fonctionel

--- dist/components/select.js
+++ dist/components/select.js
@@ -6,13 +6,21 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = Select;
 var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-function Select(props) {
+function Select(_ref) {
+  let {
+    htmlFor,
+    className,
+    children,
+    name,
+    id,
+    className2
+  } = _ref;
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("label", {
-    htmlFor: props.htmlFor,
-    className: props.className
-  }, props.children[0]), /*#__PURE__*/_react.default.createElement("select", {
-    name: props.name,
-    id: props.id,
-    className: props.className2
-  }, props.children[1]));
+    htmlFor: htmlFor,
+    className: className
+  }, children[0]), /*#__PURE__*/_react.default.createElement("select", {
+    name: name,
+    id: id,
+    className: className2
+  }, children[1]));
 }
