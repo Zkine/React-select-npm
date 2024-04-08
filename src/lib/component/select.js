1cdd4851c75714a704bede6127a0626e3ba69079
Zkine <philippe_jaymes@hotmail.com>
2024-04-08 14:10:28 +0200

    first commit

---
+++ src/lib/component/select.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Select({ props }) {
+  return (
+    <>
+      <label htmlFor={props.htmlFor} className={props.className}>
+        {props.children[0]}
+      </label>
+      <select name={props.name} id={props.id} className={props.className2}>
+        {props.children[1]}
+      </select>
+    </>
+  );
+}
